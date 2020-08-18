a66168a1916d81bd31166012378c5d481bfbe247
SebasG <gamboasebas@gmail.com>
2020-08-18 15:49:49 -0600

    start and finish node

--- PathFinder/src/app/grid/grid.component.ts
+++ PathFinder/src/app/grid/grid.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NodeComponent } from '../node/node.component';
 
 @Component({
   selector: 'app-grid',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GridComponent implements OnInit {
 
-  constructor() { }
+  board: NodeComponent[][] = [];
+  startX: number = 5;
+  startY: number = 5;
+
+  constructor() { 
+
+    for (let i = 0; i < 25; i++) {
+      let temp = []
+      for (let j = 0; j < 25; j++) {
+        const newNode = new NodeComponent();
+        temp.push(newNode);
+      }
+      this.board.push(temp);
+    }
+  }
 
   ngOnInit(): void {
+    for (let i = 0; i < this.board.length; i++) {
+      for (let j = 0; j < this.board[i].length; j++) {
+        if(i === this.startX && j === this.startY){
+          debugger;
+          this.board[i][j].isStart = true;
+        }
+        
+      }   
+    }
+  }
+
+  showBoard(){
+    console.log(this.board);
   }
 
 }
--- PathFinder/src/app/node/node.component.ts
+++ PathFinder/src/app/node/node.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 
 @Component({
   selector: 'app-node',
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NodeComponent implements OnInit {
 
-  constructor() { }
+  visited: boolean = false;
+  isStart: boolean = false;
+  isFinish: boolean = false;
+  isWall: boolean = false;
+  @Input() i: number;
+  @Input() j:number;
+  previousNode: Node;
+
+  @Output() send = new EventEmitter<boolean>();
+
+  constructor() { 
+  }
 
   ngOnInit(): void {
+    if(this.i === 5 && this.j === 5){
+      this.isStart = true;
+    }
+    else if(this.i === 20 && this.j === 20){
+      this.isFinish = true;
+    }
+  }
+
+  makeWall(){
+    console.log('si');
+    debugger;
+    this.isWall = true;
+    this.send.emit(true);
   }
 
 }
